de1e632d8f690ce2a5a595acc4a1a73f88db5d79
meng.xu <18850337241@163.com>
2016-12-12 21:07:40 +0800

    db

--- yii2-app-wechat/config/db.php
+++ yii2-app-wechat/config/db.php
@@ -4,6 +4,6 @@ return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=yii2wechat',
     'username' => 'root',
-    'password' => '',
+    'password' => '1332080218',
     'charset' => 'utf8',
 ];
