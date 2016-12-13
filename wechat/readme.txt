ab886ad084da447a88772a11394bbe117ccbde62
meng.xu <meng.xu@tpv-tech.com>
2016-12-13 08:52:46 +0800

    add yii2 extend wechat project to study

---
+++ wechat/readme.txt
@@ -0,0 +1,5 @@
+http://huajuan.me/yii2-wechat
+1. 要建立数据库
+2. php yii migrate --migrationPath=@callmez/wechat/migrations
+用户名： admin
+密码：admin
---
+++ wechat/wechat-app-advanced/wechat-app-advanced/common/config/main.php
@@ -0,0 +1,16 @@
+<?php
+return [
+    'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
+    'modules' => [
+    	// 微信模块
+	    'wechat' => [
+	        'class' => 'callmez\wechat\Module',
+	        'adminId' => 1 // 这里填写管理员ID(拥有wechat最高管理权限), 默认为第一个用户
+	    ],
+    ],
+    'components' => [
+        'cache' => [
+            'class' => 'yii\caching\FileCache',
+        ],
+    ],
+];
